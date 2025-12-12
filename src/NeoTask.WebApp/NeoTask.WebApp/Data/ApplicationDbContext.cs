40624ab93512972024f54c84e9053c05339af202
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-11 21:38:17 -0600

    switch db context responsibilities

--- src/NeoTask.WebApp/NeoTask.WebApp/Data/ApplicationDbContext.cs
+++ src/NeoTask.WebApp/NeoTask.WebApp/Data/ApplicationDbContext.cs
@@ -1,8 +1,7 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NeoTask.WebApp.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
 }
